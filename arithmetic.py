09a4fbe798da678660b3d3019ec826f32361c039
Hackbright Student <info@hackbrightacademy.com>
2014-02-05 16:24:46 -0800

    Finished arithmetic and calculator files. Yet to be bombproofed.

--- arithmetic.py
+++ arithmetic.py
@@ -11,7 +11,7 @@ def divide(num1, num2):
     return float(num1) / float(num2)
 
 def square(num1):
-    return num1 ** 2
+    return (num1 ** 2)
 
 def cube(num1):
     return num1 ** 3
--- calculator.py
+++ calculator.py
@@ -2,13 +2,36 @@ import arithmetic
 # Get input from user
 while True:
     input = raw_input("> ")
-    # Parse the input into a list
+# Parse the input into a list
     input_list = input.split(" ")
-    # Look for a quit condition and break
-    if input_list[0] == "q":
+# Look for a quit condition and break
+    operator = input_list[0]
+    if operator == "q":
         break
-    # Match any conditions to the correct function
-    if input_list[0] == "+":
+# Match any conditions to the correct function
+    if operator == "+":
         print arithmetic.add(int(input_list[1]), int(input_list[2]))
-    else:
+
+    elif operator == "-":
+        print arithmetic.subtract(int(input_list[1]), int(input_list[2]))
+
+    elif operator == "*":
+        print arithmetic.multiply(int(input_list[1]), int(input_list[2]))
+
+    elif operator == "/":
+        print arithmetic.divide(float(input_list[1]), float(input_list[2]))
+
+    elif operator == "square":
+        print arithmetic.square(float(input_list[1]))
+
+    elif operator == "cube":
+        print arithmetic.cube(float(input_list[1]))
+
+    elif operator == "pow":
+        print arithmetic.power(float(input_list[1]))
+
+    elif operator == "mod":
+        print arithmetic.mod(float(input_list[1], input_list[2]))
+
+    else: 
         print "that's not relevant input"
